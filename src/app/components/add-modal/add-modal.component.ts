53e78f51526331a7cc65367f03520b6e89c37cf1
nhnam <37695601+hoai97nam@users.noreply.github.com>
2022-09-17 16:28:44 +0700

    update reactive form

--- src/app/components/add-modal/add-modal.component.ts
+++ src/app/components/add-modal/add-modal.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
 import { CarBrandModel } from "src/app/models/car-brand.model";
 import * as uuid from 'uuid';
 import { NzUploadFile} from 'ng-zorro-antd/upload';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
     selector: 'add-modal',
@@ -16,11 +17,22 @@ export class AddModalComponent implements OnInit {
     @Output() addBrand = new EventEmitter<CarBrandModel>();
     inputValue: string | null = null;
     carBrand!: CarBrandModel;
+    addForm!: FormGroup;
 
-    constructor() {
+    constructor(
+        private formBuilder: FormBuilder
+    ) {
         this.carBrand = new CarBrandModel();
     }
     ngOnInit(): void {
+        this.addForm = this.formBuilder.group({
+            name: ['', Validators.compose([
+                Validators.minLength(3),
+                Validators.maxLength(10)
+            ])],
+            description: [''],
+            active: false
+        })
         this.carBrand.id = uuid.v4();
         this.carBrand.active = false;
         this.carBrand.models = 0;
@@ -31,6 +43,9 @@ export class AddModalComponent implements OnInit {
     }
 
     handleOk(): void {
+        // alias to model before send
+        this.carBrand.name = this.addForm.value.name;
+        this.carBrand.description = this.addForm.value.description;
         this.isVisible = false;
         this.addBrand.emit(this.carBrand);        
         this.carBrand = new CarBrandModel();
--- src/app/components/add-modal/add-modal.module.ts
+++ src/app/components/add-modal/add-modal.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { AddModalComponent } from './add-modal.component';
 import { NzDividerModule } from 'ng-zorro-antd/divider';
 import { NzInputModule } from 'ng-zorro-antd/input';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzSelectModule } from 'ng-zorro-antd/select';
@@ -18,7 +18,8 @@ import { NzUploadModule } from 'ng-zorro-antd/upload';
     NzDropDownModule,
     NzIconModule,
     NzSelectModule,
-    NzUploadModule
+    NzUploadModule,
+    ReactiveFormsModule
   ],
   declarations: [AddModalComponent],
   exports: [AddModalComponent]
